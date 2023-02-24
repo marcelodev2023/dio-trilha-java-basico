0b3b0a4b5a8eb7c9d2165517aed9a5b83aaa3007
marcelodev2023 <dev.marcelom.info@gmail.com>
2023-02-23 22:43:19 -0300

    Commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Double salarioMinimo = 2500d;
+        final double VALOR_DE_PI = 3.14;
+
+        int numero = 5;
+
+        numero++;
+
+
+        System.out.println(numero);
     }
 }
