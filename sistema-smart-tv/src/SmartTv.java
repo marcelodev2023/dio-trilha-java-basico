e3e5f43d373f5b282441c8e429f55683266cab41
marcelodev2023 <dev.marcelom.info@gmail.com>
2023-02-24 00:05:07 -0300

    smart tv

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,34 @@
+public class SmartTv {
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+    
+    public void ligarTv(){
+        this.ligada = true;
+    }
+
+    public void desligarTv(){
+        this.ligada = false;
+    }
+
+    public void aumentarVolume(){
+        this.volume++;
+    }
+
+    public void diminuirVolume(){
+        this.volume--;
+    }
+    
+    public void aumentarCanal(){
+        this.canal++;
+    }
+
+    public void diminuirCanal(){
+        this.canal--;
+    }
+
+    public void mudarCanal(int novoCanal){
+        this.canal = novoCanal;
+    }
+}
